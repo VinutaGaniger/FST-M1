b1efd5241842e4eb2311b8b3bd725a6fb18ac9fb
VinutaGaniger <132872761+VinutaGaniger@users.noreply.github.com>
2025-10-21 11:51:55 +0530

    Add files via upload

---
+++ TestNG/Activities/DemoTwo4.java
@@ -0,0 +1,12 @@
+package activities;
+
+import org.testng.annotations.Test;
+
+public class DemoTwo4 {
+	  @Test
+	    public void TestCase() {
+	        System.out.println("I'm in the test case from DemoTwo Class");
+
+}
+}
+
